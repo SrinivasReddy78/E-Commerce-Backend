cac4720cd112af20a15bb5eaa03cae8c675a6eec
SrinivasReddy78 <srinivasreddykoganti7@gmail.com>
2024-10-15 18:59:15 +0530

    feat: Express js setup

---
+++ src/types/types.ts
@@ -0,0 +1,37 @@
+export type THttpResponse = {
+    success : boolean
+    statusCode: number
+    request : {
+        ip?: string | null
+        method?: string 
+        url: string
+    }
+    message: string
+    data: unknown
+}
+
+export type THttpRequest = {
+    success : boolean
+    statusCode: number
+    request : {
+        ip?: string | null
+        method?: string 
+        url: string
+    }
+    message: string
+    data: unknown
+    trace?: object | null
+}
+
+export type THttpError = {
+    success: boolean
+    statusCode: number
+    request: {
+        ip?: string | null
+        method: string
+        url: string
+    }
+    message: string
+    data: unknown
+    trace?: object | null
+}
