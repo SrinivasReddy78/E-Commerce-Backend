da640a0a3ac0dabae710364dbdd33397adacd3ff
SrinivasReddy78 <srinivasreddykoganti7@gmail.com>
2024-10-24 12:54:10 +0530

    feat: Rate Limit Setup

--- src/constant/responseMessage.ts
+++ src/constant/responseMessage.ts
@@ -1,5 +1,6 @@
 export default {
     SUCCESS: `The Operation has been Sucessful`,
     SOMETHING_WENT_WRONG: `Something went wrong`,
-    NOT_FOUND: (entity: string)=> `${entity} not found`
+    NOT_FOUND: (entity: string)=> `${entity} not found`,
+    TOO_MANY_REQUESTS : `Too Many Requests. Try again after sometime`
 }
